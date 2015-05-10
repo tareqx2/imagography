0a4e449a37ca70a9438c3c75be9ebe1bd8183120
Tareq Perez <tareqx3@gmail.com>
2015-05-09 23:37:21 -0500

    missed an import

--- src/db.py
+++ src/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Text
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import create_session, relationship, load_only
 from sqlalchemy import create_engine
